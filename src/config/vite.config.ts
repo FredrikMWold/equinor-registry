7dabbefbd1d77ff170d067cc2373228b7e4ab58a
FredrikMWold <fredrik.wold@bouvet.no>
2025-01-31 23:43:40 +0100

    feat: update to tailwind v4

--- src/config/vite.config.ts
+++ src/config/vite.config.ts
@@ -1,10 +1,12 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: "./",
   plugins: [
+    tailwindcss(),
     react(),
     checker({
       typescript: true,
