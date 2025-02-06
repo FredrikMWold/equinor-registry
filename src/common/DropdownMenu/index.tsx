3cec7eee7544495938e008e32f1cd05138ab522c
FredrikMWold <fredrik.wold@bouvet.no>
2025-02-07 00:09:30 +0100

    fix: wrap dropdown in portal

--- src/common/DropdownMenu/index.tsx
+++ src/common/DropdownMenu/index.tsx
@@ -48,7 +48,7 @@ const DropdownMenuSubContent = React.forwardRef<
     ref={ref}
     sideOffset={sideOffset}
     className={cn(
-      `shadow-raised z-[2000] min-w-[8rem] overflow-hidden rounded-[4px]
+      `shadow-raised z-[99999] min-w-[8rem] overflow-hidden rounded-[4px]
             bg-background-default py-2 text-text-default data-[state=open]:animate-in 
             data-[state=closed]:animate-out data-[state=closed]:fade-out-0 
             data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 
@@ -68,12 +68,13 @@ const DropdownMenuContent = React.forwardRef<
   React.ElementRef<typeof DropdownMenuPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof DropdownMenuPrimitive.Content>
 >(({ className, sideOffset = 4, ...props }, ref) => (
-  <DropdownMenuPrimitive.Content
-    ref={ref}
-    sideOffset={sideOffset}
-    onCloseAutoFocus={(e) => e.preventDefault()}
-    className={cn(
-      `shadow-raised z-[2000] min-w-[8rem] overflow-hidden rounded-[4px]
+  <DropdownMenuPortal>
+    <DropdownMenuPrimitive.Content
+      ref={ref}
+      sideOffset={sideOffset}
+      onCloseAutoFocus={(e) => e.preventDefault()}
+      className={cn(
+        `shadow-raised z-[99999] min-w-[8rem] overflow-hidden rounded-[4px]
                 bg-background-default py-2 text-text-default data-[state=open]:animate-in 
                 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 
                 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 
@@ -81,10 +82,11 @@ const DropdownMenuContent = React.forwardRef<
                 data-[side=left]:slide-in-from-right-2 
                 data-[side=right]:slide-in-from-left-2 
                 data-[side=top]:slide-in-from-bottom-2`,
-      className
-    )}
-    {...props}
-  />
+        className
+      )}
+      {...props}
+    />
+  </DropdownMenuPortal>
 ));
 DropdownMenuContent.displayName = DropdownMenuPrimitive.Content.displayName;
 
